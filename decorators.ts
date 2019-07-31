2caccc6295f1572a02e67d01bffca1838cb8617a
ha <typ0@naver.com>
2019-08-01 01:21:52 +0900

    feat: decorator applyMany

--- decorators.ts
+++ decorators.ts
@@ -1,11 +1,48 @@
 import { Request } from 'express';
 
-export type Decorator = (req: Request) => any;
+export type Decorator<T> = (req: Request) => T;
 
 export const Req = (req: Request) => req;
 
 export const Body = (req: Request) => req.body;
 
-export const Param = (key: string) => (req: Request) => req.params[key] as string;
+export const Param = (key: string) => (req: Request) =>
+  req.params[key] as string;
 
-export const Query = (key: string) => (req: Request) => req.query[key] as string;
+export const Query = (key: string) => (req: Request) =>
+  req.query[key] as string;
+
+export function applyMany<A>(fnA: Decorator<A>): (x: Request) => [A];
+export function applyMany<A, B>(
+  fnA: Decorator<A>,
+  fnB: Decorator<B>,
+): (x: Request) => [A, B];
+export function applyMany<A, B, C>(
+  fnA: Decorator<A>,
+  fnB: Decorator<B>,
+  fnC: Decorator<C>,
+): (x: Request) => [A, B, C];
+export function applyMany<A, B, C, D>(
+  fnA: Decorator<A>,
+  fnB: Decorator<B>,
+  fnC: Decorator<C>,
+  fnD: Decorator<D>,
+): (x: Request) => [A, B, C, D];
+export function applyMany<A, B, C, D, E>(
+  fnA: Decorator<A>,
+  fnB: Decorator<B>,
+  fnC: Decorator<C>,
+  fnD: Decorator<D>,
+  fnE: Decorator<E>,
+): (x: Request) => [A, B, C, D, E];
+export function applyMany<A, B, C, D, E, F>(
+  fnA: Decorator<A>,
+  fnB: Decorator<B>,
+  fnC: Decorator<C>,
+  fnD: Decorator<D>,
+  fnE: Decorator<E>,
+  fnF: Decorator<F>,
+): (x: Request) => [A, B, C, D, E, F];
+export function applyMany(...decorators: Decorator<any>[]) {
+  return (req: Request) => decorators.map(decorator => decorator(req));
+}
--- example/product/product.controller.ts
+++ example/product/product.controller.ts
@@ -1,5 +1,5 @@
 import { controller } from '../../controller';
-import { Body, Param } from '../../decorators';
+import { Body, Param, applyMany } from '../../decorators';
 import * as Array from '../../fp/array';
 import * as AsyncResult from '../../fp/async-result';
 import { pipe } from '../../fp/pipe';
@@ -8,43 +8,42 @@ import { get, post, put, Route } from '../../router';
 import { ProductService } from './product.service';
 
 function ProductControllerFactory([productService]: [ProductService]): Route[] {
-    const getAll = get(
-        '',
-        pipe(
-            productService.getAll,
-            AsyncResult.match(products => ok({ products })),
-        ),
-    );
+  const getAll = get(
+    '',
+    pipe(
+      productService.getAll,
+      AsyncResult.match(products => ok({ products })),
+    ),
+  );
 
-    const getOne = get(
-        ':id',
-        pipe(
-            Param('id'),
-            id => productService.getOne(+id),
-            AsyncResult.match(products => ok({ products })),
-        ),
-    );
+  const getOne = get(
+    ':id',
+    pipe(
+      Param('id'),
+      id => productService.getOne(+id),
+      AsyncResult.match(products => ok({ products })),
+    ),
+  );
 
-    const add = post(
-        '',
-        pipe(
-            Body,
-            body => productService.add(body),
-            AsyncResult.match(id => ok({ id })),
-        ),
-    );
+  const add = post(
+    '',
+    pipe(
+      Body,
+      body => productService.add(body),
+      AsyncResult.match(id => ok({ id })),
+    ),
+  );
 
-    const edit = put(
-        ':id',
-        pipe(
-            Array.of,
-            Array.apply([Body, Param('id')]), // TODO: 이대로는 타입 추론이 안된다
-            ([body, id]) => productService.edit(+id, body),
-            AsyncResult.match(() => ok()),
-        ),
-    );
+  const edit = put(
+    ':id',
+    pipe(
+      applyMany(Body, Param('id')),
+      ([body, id]) => productService.edit(+id, body),
+      AsyncResult.match(() => ok()),
+    ),
+  );
 
-    return controller('products', [getAll, getOne, add, edit]);
+  return controller('products', [getAll, getOne, add, edit]);
 }
 
 export const ProductController = ProductControllerFactory([ProductService]);
